e7cb56002596ac62c81d0f166762040d3d6ebafa
nawakarit <272197659+nawakarit-VOID@users.noreply.github.com>
2026-06-21 12:56:13 +0700

    *keywords

--- background.js
+++ background.js
@@ -1,8 +1,36 @@
+const ignore = [
+  ".css",
+  ".png",
+  ".jpg",
+  ".jpeg",
+  ".gif",
+  ".svg",
+  ".woff",
+  ".woff2",
+  ".ttf",
+  ".ico",
+];
+
+const keywords = ["m3u8", "mpd", "token", "playlist", "manifest"];
+
 browser.webRequest.onBeforeRequest.addListener(
-    function(details) {
-        console.log(details.url);
-    },
-    {
-        urls: ["<all_urls>"]
+  (details) => {
+    const url = details.url.toLowerCase();
+
+    for (const ext of ignore) {
+      if (url.includes(ext)) {
+        return;
+      }
     }
+
+    for (const k of keywords) {
+      if (url.includes(k)) {
+        console.log("⭐", details.url);
+        return;
+      }
+    }
+  },
+  {
+    urls: ["<all_urls>"],
+  },
 );
