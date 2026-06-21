3efb59714c90da3859ac8226a7e9614aca3ed35a
nawakarit <272197659+nawakarit-VOID@users.noreply.github.com>
2026-06-21 19:53:34 +0700

    *lastMaster

--- background.js
+++ background.js
@@ -1,42 +1,15 @@
 // Copyright (c) 2026 Nawakarit
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License v3.0.
+let lastMaster = "";
 
 browser.webRequest.onBeforeRequest.addListener(
   (details) => {
-    //ถ้าเจอให้แสดง
+    if (!details.url.includes("master.m3u8")) return;
 
-    if (details.url.includes(".m3u8")) {
-      console.log("⭐", "m3u8");
-    }
+    lastMaster = details.url;
 
-    if (details.url.includes("master.m3u8")) {
-      console.log("⭐", "MASTER");
-    }
-
-    if (details.url.includes("480p.m3u8")) {
-      console.log("⭐", "480p");
-    }
-
-    if (details.url.includes("720p.m3u8")) {
-      console.log("⭐", "720p");
-    }
-
-    if (details.url.includes("1080p.m3u8")) {
-      console.log("⭐", "1080p");
-    }
-
-    if (details.url.includes("keys")) {
-      console.log("⭐", "KEYS");
-    }
-
-    const stream = {
-      url: details.url,
-      method: details.method,
-      time: Date.now(),
-    };
-
-    console.log(stream);
+    console.log(lastMaster);
   },
   {
     urls: ["<all_urls>"],
