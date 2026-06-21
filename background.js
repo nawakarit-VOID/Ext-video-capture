214844f7cdc67b723b72a1bcf7720ef2ca30c676
nawakarit <272197659+nawakarit-VOID@users.noreply.github.com>
2026-06-21 14:09:23 +0700

    *ก่อนเปลี่ยน struct

--- background.js
+++ background.js
@@ -1,22 +1,32 @@
-const ignore = [
-  ".css",
-  ".png",
-  ".jpg",
-  ".jpeg",
-  ".gif",
-  ".svg",
-  ".woff",
-  ".woff2",
-  ".ttf",
-  ".ico",
-];
-
-const keywords = ["m3u8", "mpd", "token", "playlist", "manifest"];
+// Copyright (c) 2026 Nawakarit
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License v3.0.
 
 browser.webRequest.onBeforeRequest.addListener(
   (details) => {
+    //ถ้าเจอให้แสดง
+    if (details.url.includes(".m3u8")) {
+      console.log("⭐", "m3u8");
+    }
+
     if (details.url.includes("master.m3u8")) {
-      console.log("⭐", "FOUND MASTER");
+      console.log("⭐", "MASTER");
+    }
+
+    if (details.url.includes("480p.m3u8")) {
+      console.log("⭐", "480p");
+    }
+
+    if (details.url.includes("720p.m3u8")) {
+      console.log("⭐", "720p");
+    }
+
+    if (details.url.includes("1080p.m3u8")) {
+      console.log("⭐", "1080p");
+    }
+
+    if (details.url.includes("keys")) {
+      console.log("⭐", "KEYS");
     }
 
     console.log({
